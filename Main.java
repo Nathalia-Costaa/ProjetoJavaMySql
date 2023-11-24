5a8deddf23cdb287972f4e79ccc24ea1ccf4bfb6
Nathalia-Costaa <nathaliaaparecida1804@gmail.com>
2023-11-24 16:30:11 -0300

    feat: Adicionar main

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        boolean flag = true;
+
+        while(flag) {
+            int op;
+            System.out.println("Escolha sua opção");
+            System.out.println("1 - adicionar Filme");
+            System.out.println("2 - mostrar informações do filme");
+            System.out.println("3 - mostrar informações do ator");
+            op = input.nextInt();
+            switch(op){
+
+                case 1:
+
+
+
+
+                    break;
+
+                case 2:
+
+
+
+
+                    break;
+
+                case 3:
+
+
+
+
+                    break;
+
+
+
+
+
+            }
+
+
+        }
+
+    }
+}
